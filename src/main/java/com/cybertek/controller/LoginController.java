567802a3031574ec36394b480c9ebd4531c8f017
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-11-03 16:03:46 -0700

    Ticketing Project - REST

--- src/main/java/com/cybertek/controller/LoginController.java
+++ src/main/java/com/cybertek/controller/LoginController.java
@@ -1,10 +1,32 @@
 package com.cybertek.controller;
 
+import com.cybertek.dto.UserDTO;
+import com.cybertek.entity.ResponseWrapper;
+import com.cybertek.entity.User;
+import com.cybertek.entity.common.AuthenticationRequest;
+import com.cybertek.exception.TicketingProjectException;
+import com.cybertek.mapper.MapperUtil;
+import com.cybertek.service.UserService;
+import com.cybertek.util.JWTUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
+@RequiredArgsConstructor
 public class LoginController {
+
+    private AuthenticationManager authenticationManager;
+    private UserService userService;
+    private MapperUtil mapperUtil;
+    private JWTUtil jwtUtil;
 
     @RequestMapping(value = {"/", "/login"})
     public String login(){
@@ -14,6 +36,29 @@ public class LoginController {
     @RequestMapping("/welcome")
     public String welcome(){
         return "/welcome";
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<ResponseWrapper> doLogin(@RequestBody AuthenticationRequest authenticationRequest) throws TicketingProjectException {
+
+        String username = authenticationRequest.getUsername();
+        String password = authenticationRequest.getPassword();
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        authenticationManager.authenticate(authenticationToken);
+
+        UserDTO foundUser = userService.findByUserName(username);
+        User convertedUser = mapperUtil.convert(foundUser, new User());
+
+        if (!foundUser.isEnabled()) {
+            throw new TicketingProjectException("Please verify your email");
+        }
+
+        String jwtToken = jwtUtil.generateToken(convertedUser);
+
+        return ResponseEntity
+                .ok(new ResponseWrapper("Successfully logged in", jwtToken));
     }
 
 }
