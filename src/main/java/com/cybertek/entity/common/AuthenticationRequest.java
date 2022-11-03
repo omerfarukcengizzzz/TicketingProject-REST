59c5f3334f31f4e29f3a2f200353b51a0d4a4ba2
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-11-03 15:20:52 -0700

    Ticketing Project - REST

--- src/main/java/com/cybertek/entity/common/AuthenticationRequest.java
+++ src/main/java/com/cybertek/entity/common/AuthenticationRequest.java
@@ -1,4 +1,15 @@
 package com.cybertek.entity.common;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class AuthenticationRequest {
+
+    private String username;
+    private String password;
+
 }
