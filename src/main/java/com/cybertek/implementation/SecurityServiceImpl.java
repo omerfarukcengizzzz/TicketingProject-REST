264effaa535fe02fe793b12f3efbed5d0273bef7
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-11-02 20:32:01 -0700

    Ticketing Project - REST

--- src/main/java/com/cybertek/implementation/SecurityServiceImpl.java
+++ src/main/java/com/cybertek/implementation/SecurityServiceImpl.java
@@ -1,32 +1,50 @@
 package com.cybertek.implementation;
 
+import com.cybertek.dto.UserDTO;
 import com.cybertek.entity.User;
-import com.cybertek.entity.common.UserPrincipal;
-import com.cybertek.repository.UserRepository;
+import com.cybertek.mapper.MapperUtil;
 import com.cybertek.service.SecurityService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.cybertek.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class SecurityServiceImpl implements SecurityService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
+    private MapperUtil mapperUtil;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUserName(username);
+        UserDTO user = userService.findByUserName(username);
 
         if (user == null) throw new UsernameNotFoundException("This user does not exists!");
 
-        return new UserPrincipal(user);
+        return new org.springframework.security.core.userdetails.User(user.getId().toString(), user.getPassword(), listAuthorities(user));
+    }
+
+    private Collection<? extends GrantedAuthority> listAuthorities(UserDTO user){
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().getDescription());
+        authorityList.add(authority);
+
+        return authorityList;
     }
 
     @Override
     public User loadUser(String param) throws AccessDeniedException {
-        return null;
+        return mapperUtil.convert(userService.findByUserName(param), new User());
     }
+
 }
