1f5ae923404d4bf5d51e95d6852d6a4b634768b0
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-11-02 17:02:19 -0700

    Ticketing Project - REST

--- src/main/java/com/cybertek/config/SecurityConfiguration.java
+++ src/main/java/com/cybertek/config/SecurityConfiguration.java
@@ -2,10 +2,13 @@ package com.cybertek.config;
 
 import com.cybertek.service.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -16,6 +19,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private SecurityService securityService;
     @Autowired
     private AuthSuccessHandler authSuccessHandler;
+//    @Autowired
+//    private SecurityFilter securityFilter;
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -54,7 +65,32 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .key("CybertekSecret")
                 .userDetailsService(securityService);   // it will create a remember-me cookie based on the user returned from this class
 
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(permittedUrls)
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+
+        // run this security filter before any API call
+//        http
+//                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
     }
+
+    private static final String[] permittedUrls = {
+            "/authenticate",
+            "/create-user",
+            "/api/p1/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/webjars/**"
+    };
 
 }
