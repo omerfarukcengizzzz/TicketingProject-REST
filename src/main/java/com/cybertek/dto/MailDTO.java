6dfdd14e678da6e371d93539a1613562d8e5c451
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-11-15 15:50:54 -0800

    Ticketing Project - REST

--- src/main/java/com/cybertek/dto/MailDTO.java
+++ src/main/java/com/cybertek/dto/MailDTO.java
@@ -1,4 +1,17 @@
 package com.cybertek.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class MailDTO {
+
+    private String emailTo;
+    private String emailFrom;
+    private String message;
+    private String token;
+    private String subject;
+    private String url;
+
 }
