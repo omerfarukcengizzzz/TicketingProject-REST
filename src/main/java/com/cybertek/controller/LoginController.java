aa76db691726d5951f69ca01e8da5fc3b3d1f75e
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-11-17 10:30:16 -0800

    Ticketing Project - REST

--- src/main/java/com/cybertek/controller/LoginController.java
+++ src/main/java/com/cybertek/controller/LoginController.java
@@ -1,17 +1,21 @@
 package com.cybertek.controller;
 
 import com.cybertek.annotation.DefaultExceptionMessage;
+import com.cybertek.dto.MailDTO;
 import com.cybertek.dto.UserDTO;
+import com.cybertek.entity.ConfirmationToken;
 import com.cybertek.entity.ResponseWrapper;
 import com.cybertek.entity.User;
 import com.cybertek.entity.common.AuthenticationRequest;
 import com.cybertek.exception.TicketingProjectException;
+import com.cybertek.service.ConfirmationTokenService;
 import com.cybertek.util.MapperUtil;
 import com.cybertek.service.UserService;
 import com.cybertek.util.JWTUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,6 +27,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Authentication Controller", description = "Authenticate API")
 public class LoginController {
 
+    @Value("${app.local-url}")
+    private String BASE_URL;
+
+
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
@@ -31,6 +39,8 @@ public class LoginController {
     private MapperUtil mapperUtil;
     @Autowired
     private JWTUtil jwtUtil;
+    @Autowired
+    private ConfirmationTokenService confirmationTokenService;
 
     @PostMapping("/authenticate")
     @DefaultExceptionMessage(defaultMessage = "Bad Credentials")
@@ -60,12 +70,31 @@ public class LoginController {
     @DefaultExceptionMessage(defaultMessage = "Something went wrong, try again!")
     @PostMapping("/create-user")
     @Operation(summary = "Create new account")
-    private ResponseEntity<ResponseWrapper> doRegister(@RequestBody UserDTO userDTO) {
+    private ResponseEntity<ResponseWrapper> doRegister(@RequestBody UserDTO userDTO) throws TicketingProjectException {
 
         UserDTO createdUser = userService.save(userDTO);
 
 
 
+    }
+
+    // a custom method to create an email
+    private MailDTO createEmail(UserDTO userDTO) {
+
+        User user = mapperUtil.convert(userDTO, new User());
+        ConfirmationToken confirmationToken = new ConfirmationToken(user);
+
+        confirmationToken.setIsDeleted(false);
+
+        ConfirmationToken createdConfirmationToken = confirmationTokenService.save(confirmationToken);
+
+        return MailDTO.builder()
+                .emailTo(user.getUserName())
+                .token(createdConfirmationToken.getToken())
+                .subject("Confirm Registration")
+                .message("To confirm your account, please click here:")
+                .url(BASE_URL + "/confirmation?token=")
+                .build();
     }
 
 }
