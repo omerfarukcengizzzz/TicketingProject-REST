847b7ad78f30523f753e56ee718eb67eb8e1d635
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-11-18 14:07:16 -0800

    Ticketing Project - REST

--- src/main/java/com/cybertek/implementation/UserServiceImpl.java
+++ src/main/java/com/cybertek/implementation/UserServiceImpl.java
@@ -151,6 +151,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO confirm(User user) {
-        return null;
+        user.setEnabled(true);
+        User confirmedUser = userRepository.save(user);
+        return mapperUtil.convert(confirmedUser, new UserDTO());
     }
 }
