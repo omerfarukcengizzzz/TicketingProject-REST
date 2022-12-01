49e968890339390661c923b2b2c29e604cf67dd1
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-12-01 13:49:04 -0800

    Ticketing Project - REST

--- src/main/java/com/cybertek/entity/Project.java
+++ src/main/java/com/cybertek/entity/Project.java
@@ -1,6 +1,7 @@
 package com.cybertek.entity;
 
 import com.cybertek.enums.Status;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @Where(clause = "is_deleted = false")
+@JsonIgnoreProperties(value = {"HibernateLazyInitializer"}, ignoreUnknown = true)
 public class Project extends BaseEntity {
 
     private String projectName;
