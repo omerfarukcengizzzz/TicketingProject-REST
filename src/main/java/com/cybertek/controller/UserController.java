47e60c31b2f83ffc20a68d4e9911f89bf0351b16
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-12-01 14:03:45 -0800

    Ticketing Project - REST

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -1,6 +1,26 @@
 package com.cybertek.controller;
 
+import com.cybertek.annotation.DefaultExceptionMessage;
+import com.cybertek.dto.MailDTO;
+import com.cybertek.dto.UserDTO;
+import com.cybertek.entity.ConfirmationToken;
+import com.cybertek.entity.ResponseWrapper;
+import com.cybertek.entity.User;
+import com.cybertek.exception.TicketingProjectException;
+import com.cybertek.service.ConfirmationTokenService;
+import com.cybertek.service.RoleService;
+import com.cybertek.service.UserService;
+import com.cybertek.util.JWTUtil;
+import com.cybertek.util.MapperUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +29,60 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "User Controller", description = "User API")
 public class UserController {
 
+    @Value("${app.local-url}")
+    private String BASE_URL;
 
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private MapperUtil mapperUtil;
+    @Autowired
+    private RoleService roleService;
+    @Autowired
+    private ConfirmationTokenService confirmationTokenService;
+
+    @DefaultExceptionMessage(defaultMessage = "Something went wrong, try again!")
+    @PostMapping("/create-user")
+    @Operation(summary = "Create new account")
+    private ResponseEntity<ResponseWrapper> doRegister(@RequestBody UserDTO userDTO) throws TicketingProjectException {
+
+        UserDTO createdUser = userService.save(userDTO);
+
+        sendEmail(createEmail(createdUser));
+
+        return ResponseEntity.ok(new ResponseWrapper("User has been created!", createdUser));
+    }
+
+
+    // Custom methods
+    private MailDTO createEmail(UserDTO userDTO) {
+
+        User user = mapperUtil.convert(userDTO, new User());
+
+        ConfirmationToken confirmationToken = new ConfirmationToken(user);
+
+        confirmationToken.setIsDeleted(false);
+
+        ConfirmationToken createdConfirmationToken = confirmationTokenService.save(confirmationToken);
+
+        return MailDTO.builder()
+                .emailTo(user.getUserName())
+                .token(createdConfirmationToken.getToken())
+                .subject("Confirm Registration")
+                .message("To confirm your account, please click here:")
+                .url(BASE_URL + "/confirmation?token=")
+                .build();
+    }
+
+    // a custom method to send the email
+    private void sendEmail(MailDTO mailDTO) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setTo(mailDTO.getEmailTo());
+        mailMessage.setSubject(mailDTO.getSubject());
+        mailMessage.setText(mailDTO.getMessage() + mailDTO.getUrl() + mailDTO.getToken());
+
+        confirmationTokenService.sendEmail(mailMessage);
+    }
 
 }
