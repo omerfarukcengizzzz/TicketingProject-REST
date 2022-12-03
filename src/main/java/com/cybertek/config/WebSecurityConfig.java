769f36b5f385c0cc31147c60f0baf1086501ed70
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-12-02 20:52:04 -0800

    Ticketing Project - REST

--- src/main/java/com/cybertek/config/WebSecurityConfig.java
+++ src/main/java/com/cybertek/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -34,7 +35,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(permittedUrls)
                 .permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);        // stateless means that the browser will not keep the cookies (JSESSIONID)
 
 
         // run this security filter before any API call
