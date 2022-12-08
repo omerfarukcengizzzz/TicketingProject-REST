cbca7184e903c04e291abc2eb11dd117cca54f5a
Omer Faruk <omerfarukcengizzzz@gmail.com>
2022-12-07 20:13:38 -0800

    Ticketing Project - REST

---
+++ src/main/java/com/cybertek/aspects/LoggingAspect.java
@@ -0,0 +1,29 @@
+package com.cybertek.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Aspect
+@Configuration
+public class LoggingAspect {
+
+    Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Pointcut("execution(* com.cybertek.controller.ProjectController.*()) || execution(* com.cybertek.controller.TaskController.*())")
+    private void anyControllerOperation() {}
+
+    @Before("anyControllerOperation()")
+    public void anyBeforeControllerOperationAdvice(JoinPoint joinPoint) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        logger.info("Before(User : {} - Method : {} - Parameters : {})", authentication.getName(), joinPoint.getSignature().toShortString(), joinPoint.getArgs());
+    }
+    
+}
